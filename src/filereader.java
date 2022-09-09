c4a2ea83a9cf34c0ee58214d3a583b36928ffcfb
IsacNti <77719323+IsacNti@users.noreply.github.com>
2022-09-09 13:46:27 +0200

    på arraylist

--- src/filereader.java
+++ src/filereader.java
@@ -1,30 +1,52 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class filereader {
+
+
+
     public static void main(String[] args) {
-        BufferedWriter inFile = openTextFileReadning ("info.text");
+        BufferedReader inFile = openTextFileReadning ("info.text");
         ArrayList<String> fileContent = readAllText(inFile);
-        DataOutputStream outFile = openBinaryFileForWriting("info.dat");
-        writeTextAsBinary(fileContent);
+       // DataOutputStream outFile = openBinaryFileForWriting("info.dat");
+       // writeTextAsBinary(fileContent);
+        System.out.println(inFile);
     }
 
 
-    private static BufferedWriter openTextFileReadning(String fileName) {
+    private static BufferedReader openTextFileReadning(String fileName) {
         BufferedReader in = null;
-        try{
+        try {
+            FileReader fr = new FileReader(fileName);
+            BufferedReader inFil = new BufferedReader(fr);
+            return inFil;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+        private static ArrayList<String> readAllText(BufferedReader inFile) {
+
+            ArrayList<String> File = new ArrayList<String>();
+            try {
+                while (inFile.readLine() != null)
+                {
+                    File.add(inFile.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(File);
+            return File;
     }
 
-    private static ArrayList<String> readAllText(BufferedWriter inFile) {
-    }
-
-    private static DataOutputStream openBinaryFileForWriting(String s) {
+    /*private static DataOutputStream openBinaryFileForWriting(String s) {
     }
 
     private static void writeTextAsBinary(ArrayList<String> fileContent) {
     }
+
+     */
 }
-}
+//}
