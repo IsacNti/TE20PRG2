1cfc538ae4081f5cec3517b0e3b3621f01def585
IsacNti <77719323+IsacNti@users.noreply.github.com>
2022-09-23 13:38:46 +0200

    Ui

---
+++ src/ok.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class ok {
+    private JPanel ok;
+    private JButton button1;
+    private JTextPane textPane1;
+    private JButton button2;
+    private JPasswordField passwordField1;
+}
