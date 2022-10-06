041616152753bb9e17cd4890596f34d037191213
IsacNti <77719323+IsacNti@users.noreply.github.com>
2022-10-06 13:41:59 +0200

    cryptering fixad fixar decryptering i metod

--- src/Kryperingsprogram/model.java
+++ src/Kryperingsprogram/model.java
@@ -1,16 +1,56 @@
 package Kryperingsprogram;
 
 public class model {
-    public static void main(String[] args) {
 
-        int teck = 'a';
-        int key = '-';
-        String getbinaryKey = Integer.toBinaryString(key);
-        String getBinaryTeck = Integer.toBinaryString(teck);
-        int IntBinaryTeck = Integer.parseInt(getbinaryKey,2);
-        int IntBinaryKey = Integer.parseInt(getBinaryTeck,2);
-        int xorTeck = IntBinaryTeck^IntBinaryKey;
-        System.out.println(xorTeck);
+
+    public String cryptString(String message, String key){
+        String result = "";
+        while (key.length() < message.length()){
+            key = expandkey(key);
+        }
+        for (int i = 0 ; i < message.length(); i++){
+            result += (char)crypt(message.charAt(i), key.charAt(i));
+        }
+        return result;
+    }
+
+    private int crypt(int m, int k) {
+        return m^k;
+    }
+
+
+    private String expandkey(String key) {
+        return key+key;
+    }
+
+
+    public String decryptString(String message, String key){
+        String result = "";
+        while (key.length() < message.length()){
+            key = deexpandkey(key);
+        }
+        for (int i = 0 ; i < message.length(); i++){
+            result += (char)decrypt(message.charAt(i), key.charAt(i));
+        }
+        return result;
+    }
+
+    private int decrypt(int m, int k) {
+        return m^k;
+    }
+
+
+    private String deexpandkey(String key) {
+        return key+key;
+    }
+
+    public static void main(String[] args) {
+        String message = "Tjena";
+        String key = "ㅜㅌ20";
+        model cryptmodel = new model();
+        System.out.println(cryptmodel.cryptString(message, key));
+        model decryptmodel = new model();
+
     }
 
 
